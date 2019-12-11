75c569f8a4d8b9c0c2d61876753142fcc4f06189
Abegemot <marcha64@yahoo.com>
2019-12-11 09:03:39 +0100

    Change to dev3

---
+++ buildSrc/src/main/java/Deps.java
@@ -0,0 +1,3 @@
+public class Deps {
+    public static final String jcompose="0.1.0-dev03";
+}
--- kclib/src/main/java/com/begemot/kclib/kclib.kt
+++ kclib/src/main/java/com/begemot/kclib/kclib.kt
@@ -6,9 +6,10 @@ import androidx.compose.ambient
 import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.core.*
+
 import androidx.ui.foundation.shape.RectangleShape
-import androidx.ui.foundation.shape.border.Border
-import androidx.ui.input.EditorStyle
+//import androidx.ui.input.EditorStyle
+
 import androidx.ui.layout.*
 import androidx.ui.material.surface.Surface
 import androidx.ui.core.dp
@@ -24,115 +25,113 @@ fun hellokclib():String="kkdkdkdk"
 fun Test(){
     Text("Hola")
 }
-
-
 @Preview
-@Composable fun testKContainer(){
-    val count = +state { 0 }
-    val context = +ambient(ContextAmbient)
-  // Text("UNI")
-KWindow() {
-    KContainer {
-        KHeader("logins ")
-        KField("email")
-        Kline()
-        KField("password")
-        HeightSpacer(8.dp)
-         KButtonBar {
-              Button(text = "Loggin",onClick = { Toast.makeText(null, "jsjsjs", Toast.LENGTH_SHORT).show() })
-              Button(text = "Register", onClick = { count.value += 1; })
-          }
+@Composable
+fun TestKWindow(){
+    KWindow {
+        Text("Hola")
     }
 }
+@Preview
+@Composable
+fun TestKContainer(){
+    KWindow() {
+        KContainer {
+            Text("Hola")
+        }
+    }
 }
 
-
+@Preview
+@Composable fun testKContainer2(){
+    val context = +ambient(ContextAmbient)
+    val kt = +state {kTheme.NONE}
+    KWindow(kt.value) {
+        KContainer {
+            KHeader("Login")
+            KField("e-mail")
+            Kline()
+            KField("password")
+            //HeightSpacer(8.dp)
+            KButtonBar {
+                  Button(text = "Loggin",onClick = { Toast.makeText(null, "jsjsjs", Toast.LENGTH_SHORT).show() })
+                  Button(text = "Register", onClick = { kt.value = kTheme.DARK; })
+            }
+        }
+    }
+}
 
 @Composable
-fun KWindow  (kt:kTheme= kTheme.LIGHT,children:@Composable ()()->Unit){
+fun KWindow  (kt:kTheme= kTheme.DARK  ,children:@Composable ()()->Unit){
+    //val kt2 = +state {kt}
     MaterialTheme(colors = kt.theme) {
         children()
     }
 }
 @Composable fun KContainer(children: @Composable() () -> Unit) {
-    // Container(width = 140.dp) {
-    //      Surface(
-    // shape = RoundedCornerShape(1.dp),
-    // color = +themeColor { surface },
-    //              color = Color.Gray,
-    //              border = Border(+themeColor { primaryVariant }, 1.dp),
-    //            elevation = 1.dp
-    //     ) {
-    Column(crossAxisAlignment = CrossAxisAlignment.Stretch ) {
-        children()
+     Container(width = 270.dp) {
+       Column{
+       children()
     }
-    //    }
-//    }
+  }
 }
 
 @Composable fun Kline(){
     Surface(color= Color.DarkGray) {
-        Container(expanded = false, height = 1.dp, width = 150.dp){}
-    }
-}
-
-@Composable fun KButtonBar(children: @Composable() () -> Unit) {
-    Surface(color =+themeColor { primaryVariant } /*, shape = RectangleShape,border = Border(Color.Red,1.dp)*/) {
-        Padding(padding = EdgeInsets(top = 6.dp,bottom = 6.dp)) {
-            Container(expanded = false, width = 270.dp, alignment = Alignment.CenterLeft) {
-                FlexRow(mainAxisAlignment = MainAxisAlignment.SpaceEvenly) {
-                    inflexible {
-                        children()
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable fun KField(txt:String){
-    val state = +state { EditorModel("North Poles") }
-    Surface(color = +themeColor { surface }//,
-            // shape = RectangleShape,border = Border(Color.Red,0.dp)
-    ) {
-        //      Padding(12.dp) {
-        Row(crossAxisAlignment = CrossAxisAlignment.Center) {
-            Column {
-                Padding(padding = 6.dp) {
-                    Text(text = txt, style = (+themeTextStyle { caption }).withOpacity(0.65f))
-                }
-                val v = themeTextStyle { h2 }
-                Container(expanded = false, height = 34.dp, width = 270.dp,padding = EdgeInsets(left=12.dp)) {
-                   TextField(
-                      value = state.value,
-                      onValueChange = { state.value = it },
-                          editorStyle = EditorStyle(
-                                    /*textStyle = TextStyle(
-                                        fontSize = (20.sp)
-                                    )*/
-                                    textStyle = +themeTextStyle { h5 }
-                            )
-                    )
-                }
-            }
-
-        }
-        //      }
-
+        Container(expanded = true, height = 1.dp){}
     }
 }
 
 @Composable fun KHeader(txt:String){
-    Surface(color =+themeColor { primary } , shape = RectangleShape,border = Border(Color.Yellow,0.dp)) {
-        Container(expanded = false, height = 54.dp, width = 270.dp,alignment = Alignment.CenterLeft) {
+   // Surface(color =(+MaterialTheme.colors()).primary , shape = RectangleShape,border = Border(Color.Yellow,0.dp)) {
+    Surface(color =(+MaterialTheme.colors()).primary , shape = RectangleShape) {
+        Container(modifier = ExpandedWidth, alignment = Alignment.CenterLeft) {
             Padding(padding = 12.dp) {
-                Text(text = txt, style = (+themeTextStyle { h5 }).withOpacity(0.97f))
+                   Text(text = txt, style = (+MaterialTheme.typography()).h5.withOpacity(0.97f))
             }
         }
     }
 }
+@Composable fun KField(txt:String){
+    val state = +state { EditorModel("North Poles") }
+    Surface(color = (+MaterialTheme.colors()).surface //,
+            // shape = RectangleShape,border = Border(Color.Red,0.dp)
+    ) {
+      Row() {
+            Column {
+                Padding(padding = EdgeInsets(left=6.dp,top = 4.dp)) {
+                    Text(text = txt, style = (+MaterialTheme.typography()).caption.withOpacity(0.65f))
+                }
+                Container(modifier = ExpandedWidth,padding = EdgeInsets(left=12.dp,bottom = 4.dp)) {
+                    TextField(
+                            value = state.value,
+                            onValueChange = { state.value = it },
+                            textStyle = (+MaterialTheme.typography()).h6
+                      )
+                }
+            }
+        }
+   }
+}
 
-val lightThemeColors = MaterialColors(
+
+
+@Composable fun KButtonBar(children: @Composable() () -> Unit) {
+    Surface(color =(+MaterialTheme.colors()).primaryVariant   /*, shape = RectangleShape,border = Border(Color.Red,1.dp)*/) {
+        Padding(padding = EdgeInsets(10.dp)) {
+            Container(expanded = false,  alignment = Alignment.Center) {
+                  FlexRow(mainAxisAlignment = MainAxisAlignment.SpaceEvenly,modifier = ExpandedWidth){
+                  inflexible(children)
+                }
+            }
+        }
+     }
+}
+
+
+
+
+val lightThemeColors = ColorPalette(
         primary = Color(0xFFDD0D3C),
         primaryVariant = Color(0xFFC20029),
         onPrimary = Color.White,
@@ -145,7 +144,7 @@ val lightThemeColors = MaterialColors(
         error = Color(0xFFD00036),
         onError = Color.White
 )
-val darkThemeColors = MaterialColors(
+val darkThemeColors = ColorPalette(
         primary = Color(0xFFEA6D7E),
         primaryVariant = Color(0xFFDD0D3E),
         onPrimary = Color.Black,
@@ -157,8 +156,15 @@ val darkThemeColors = MaterialColors(
         onSurface = Color.White
 )
 
-val klistThemes=listOf<MaterialColors>(lightThemeColors,darkThemeColors)
 
-enum class kTheme(val theme:MaterialColors){
-    DARK(darkThemeColors),LIGHT(lightThemeColors),NONE(MaterialColors())
+enum class kTheme(val theme:ColorPalette){
+    DARK(darkThemeColors),LIGHT(lightThemeColors),NONE(ColorPalette());
+    companion object {
+        fun next(kt: kTheme): kTheme {
+            val i = kTheme.values().size
+            var nexttheme = kt.ordinal + 1
+            if (nexttheme == i) nexttheme = 0
+            return kTheme.values().get(nexttheme)
+        }
+    }
 }
