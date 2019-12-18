1f3a36991065b7a332c88282354631a4a3a752e6
Abegemot <marcha64@yahoo.com>
2019-12-18 13:00:06 +0100

    second commit

--- kclib/src/main/java/com/begemot/kclib/kclib.kt
+++ kclib/src/main/java/com/begemot/kclib/kclib.kt
@@ -1,170 +1,196 @@
 package com.begemot.kclib
 
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.compose.Composable
-import androidx.compose.ambient
+import androidx.compose.*
 import androidx.compose.state
-import androidx.compose.unaryPlus
 import androidx.ui.core.*
 
 import androidx.ui.foundation.shape.RectangleShape
-//import androidx.ui.input.EditorStyle
 
 import androidx.ui.layout.*
 import androidx.ui.material.surface.Surface
 import androidx.ui.core.dp
+import androidx.ui.foundation.Clickable
+import androidx.ui.foundation.SimpleImage
 import androidx.ui.material.*
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.graphics.Color
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.input.ImeAction
+import androidx.ui.input.KeyboardType
 
 
 fun hellokclib():String="kkdkdkdk"
 
-@Preview
-@Composable
-fun Test(){
-    Text("Hola")
-}
-@Preview
-@Composable
-fun TestKWindow(){
-    KWindow {
-        Text("Hola")
-    }
-}
-@Preview
-@Composable
-fun TestKContainer(){
-    KWindow() {
-        KContainer {
-            Text("Hola")
-        }
-    }
-}
 
-@Preview
-@Composable fun testKContainer2(){
-    val context = +ambient(ContextAmbient)
-    val kt = +state {kTheme.NONE}
-    KWindow(kt.value) {
-        KContainer {
-            KHeader("Login")
-            KField("e-mail")
-            Kline()
-            KField("password")
-            //HeightSpacer(8.dp)
-            KButtonBar {
-                  Button(text = "Loggin",onClick = { Toast.makeText(null, "jsjsjs", Toast.LENGTH_SHORT).show() })
-                  Button(text = "Register", onClick = { kt.value = kTheme.DARK; })
+
+@Composable
+fun KWindow  (kt:kTheme= kTheme.NONE, size:Int, children:@Composable ()()->Unit){
+    MaterialTheme(colors = kt.theme) {
+        Surface(color = (+MaterialTheme.colors()).secondary,shape = RectangleShape) {
+        Container(width = size.dp,alignment = Alignment.TopLeft) {
+            Column {  children() }
             }
         }
     }
 }
 
-@Composable
-fun KWindow  (kt:kTheme= kTheme.DARK  ,children:@Composable ()()->Unit){
-    //val kt2 = +state {kt}
-    MaterialTheme(colors = kt.theme) {
-        children()
+@Composable fun KText(txt:String) {
+    Surface(color = (+MaterialTheme.colors()).surface) {
+        Container (modifier = ExpandedWidth,alignment = Alignment.CenterLeft  ) {
+            Padding(padding = EdgeInsets(left = 6.dp, top = 4.dp,bottom = 4.dp)) {
+                Text(text = txt, style = (+MaterialTheme.typography()).caption.withOpacity(0.65f))
+            }
+        }
     }
-}
-@Composable fun KContainer(children: @Composable() () -> Unit) {
-     Container(width = 270.dp) {
-       Column{
-       children()
-    }
-  }
 }
 
 @Composable fun Kline(){
     Surface(color= Color.DarkGray) {
-        Container(expanded = true, height = 1.dp){}
+        Container(modifier = ExpandedWidth, height = 1.dp){}
     }
 }
 
-@Composable fun KHeader(txt:String){
-   // Surface(color =(+MaterialTheme.colors()).primary , shape = RectangleShape,border = Border(Color.Yellow,0.dp)) {
+@Composable fun KHeader(txt:String, onClick: (() -> Unit)? = null){
+    val ctx=+ambient(ContextAmbient)
     Surface(color =(+MaterialTheme.colors()).primary , shape = RectangleShape) {
-        Container(modifier = ExpandedWidth, alignment = Alignment.CenterLeft) {
+        Container(modifier = ExpandedWidth,alignment = Alignment.CenterLeft) {
             Padding(padding = 12.dp) {
-                   Text(text = txt, style = (+MaterialTheme.typography()).h5.withOpacity(0.97f))
+                Row(modifier = ExpandedWidth) {
+                    Row(modifier = Flexible(1f)){
+                        Text(text = txt  , style = (+MaterialTheme.typography()).h5.withOpacity(0.97f))
+                    }
+                    Clickable(onClick = onClick) {
+                        val img= imageFromResource(ctx.resources,R.drawable.ic_icons8_ruleta_48)
+                        SimpleImage(image = img)
+                     }
+                }
             }
         }
     }
 }
-@Composable fun KField(txt:String){
-    val state = +state { EditorModel("North Poles") }
-    Surface(color = (+MaterialTheme.colors()).surface //,
-            // shape = RectangleShape,border = Border(Color.Red,0.dp)
-    ) {
-      Row() {
-            Column {
-                Padding(padding = EdgeInsets(left=6.dp,top = 4.dp)) {
-                    Text(text = txt, style = (+MaterialTheme.typography()).caption.withOpacity(0.65f))
-                }
-                Container(modifier = ExpandedWidth,padding = EdgeInsets(left=12.dp,bottom = 4.dp)) {
-                    TextField(
-                            value = state.value,
-                            onValueChange = { state.value = it },
-                            textStyle = (+MaterialTheme.typography()).h6
-                      )
-                }
+
+
+@Composable fun KField(txt:String, st:State<String>){
+    val context=+ambient(ContextAmbient)
+    Surface(color = (+MaterialTheme.colors()).surface) {
+        Container(modifier = ExpandedWidth) {
+            Column() {
+                Padding(padding = EdgeInsets(left = 6.dp, top = 4.dp)) {
+                Text(text = txt, style = (+MaterialTheme.typography()).caption.withOpacity(0.65f))
             }
+            Row(){
+                    WidthSpacer(12.dp)
+                        TextField(
+                            value = st.value,
+                            onValueChange = { st.value = it },
+                            textStyle = (+MaterialTheme.typography()).h6,
+                            keyboardType = KeyboardType.Text,
+                            onImeActionPerformed = {
+                                if (it == ImeAction.Done) {
+                                       hideKeyboard(context as Activity)
+                                  }
+                               },
+                            imeAction = ImeAction.Done
+                             )
+            }
+            HeightSpacer(3.dp)
+           }
         }
-   }
+    }
+}
+
+fun hideKeyboard(activity: Activity) {
+    val imm: InputMethodManager = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    var view = activity.currentFocus
+    if (view == null) {
+        view = View(activity)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
 
 
-
+/*
+Button bar goes wrong when too many buttons and too few space but by now .....
+ */
 @Composable fun KButtonBar(children: @Composable() () -> Unit) {
     Surface(color =(+MaterialTheme.colors()).primaryVariant   /*, shape = RectangleShape,border = Border(Color.Red,1.dp)*/) {
         Padding(padding = EdgeInsets(10.dp)) {
-            Container(expanded = false,  alignment = Alignment.Center) {
-                  FlexRow(mainAxisAlignment = MainAxisAlignment.SpaceEvenly,modifier = ExpandedWidth){
-                  inflexible(children)
+            Container(modifier = ExpandedWidth) {
+                FlexRow(mainAxisAlignment = MainAxisAlignment.SpaceEvenly,modifier = ExpandedWidth) {
+                    inflexible(children)
                 }
             }
         }
-     }
+    }
 }
 
 
+/*
+PREVIEWS
+ */
+
+@Preview
+@Composable
+fun TWindow(){
+    KWindow(size=150) {
+        Text("Holaq")
+    }
+}
+
+@Preview
+@Composable
+fun THeader(){
+    KWindow(size=150) {
+        KHeader("H")
+    }
+}
 
 
-val lightThemeColors = ColorPalette(
-        primary = Color(0xFFDD0D3C),
-        primaryVariant = Color(0xFFC20029),
-        onPrimary = Color.White,
-        secondary = Color.White,
-        onSecondary = Color.Black,
-        background = Color.White,
-        onBackground = Color.Black,
-        surface = Color.White,
-        onSurface = Color.Black,
-        error = Color(0xFFD00036),
-        onError = Color.White
-)
-val darkThemeColors = ColorPalette(
-        primary = Color(0xFFEA6D7E),
-        primaryVariant = Color(0xFFDD0D3E),
-        onPrimary = Color.Black,
-        secondary = Color(0xFF121212),
-        onSecondary = Color.White,
-        surface = Color(0xFF121212),
-        background = Color(0xFF121212),
-        onBackground = Color.White,
-        onSurface = Color.White
-)
+@Preview
+@Composable
+fun THeaderField  (){
+    val s= +state{"test header"}
+    KWindow(size=200) {
+        KHeader("Login")
+        KField("e-mail",s)
+    }
+}
+@Preview
+@Composable fun TLogin() {
+    val s1 = +state { "myemail@com" }
+    val s2 = +state { "mypassword" }
+    KWindow(kTheme.NONE,size=222) {
+        KHeader(txt = "Login")
+        KField("e-mail", s1)
+        Kline()
+        KField("password", s2)
+        KButtonBar {
+            Button(text = "Loggin", onClick = { Toast.makeText(null, "Login clicked", Toast.LENGTH_SHORT).show() })
+            Button(text = "Register", onClick = { })
+        }
+    }
+}
 
-
-enum class kTheme(val theme:ColorPalette){
-    DARK(darkThemeColors),LIGHT(lightThemeColors),NONE(ColorPalette());
-    companion object {
-        fun next(kt: kTheme): kTheme {
-            val i = kTheme.values().size
-            var nexttheme = kt.ordinal + 1
-            if (nexttheme == i) nexttheme = 0
-            return kTheme.values().get(nexttheme)
+@Preview
+@Composable fun TAll() {
+    val s = +state { "test with long text ssssssssssssssssssssssssssssssssssssssss xxx" }
+    KWindow(kTheme.NONE,size=222) {
+        Text("exploring limits where size components bigger than kwindow size")
+        Text("some more  2lllllllllllllllllllllll xxxxxxxxxxxxx llllllllllllllllllzq")
+        KHeader(txt = "Login")
+        KField("e-mail", s)
+        Kline()
+        KText("ktext ")
+        Kline()
+        KField("password", s)
+        KButtonBar {
+            Button(text = "Loggin", onClick = { Toast.makeText(null, "jsjsjs", Toast.LENGTH_SHORT).show() })
+            Button(text = "Register", onClick = { })
+            Button(text = "Register2", onClick = { })
+            Button(text = "Register23", onClick = { })
         }
     }
 }
